7716c0f9c84d7afd63a9a7ed937df713bc5017c0
Rein Appeldoorn <reinzor@gmail.com>
2019-04-01 08:19:08 +0200

    new: parameters in node namespace

--- src/ros_mac_authentication.cpp
+++ src/ros_mac_authentication.cpp
@@ -26,7 +26,7 @@ using namespace ros;
  * The ROS parameter name for the file that contains the secret string. We do not store the actual
  * string in the parameter server as the parameter server itself may not be secure.
  */
-#define SECRET_FILE_PARAM "/ros_mac_authentication/secret_file_location"
+#define SECRET_FILE_PARAM "secret_file_location"
 
 /*!
  * \def MISSING_PARAMETER
@@ -106,10 +106,11 @@ int main(int argc, char **argv)
   // initialize ROS and the node
   init(argc, argv, "ros_mac_authentication");
   NodeHandle node;
+  NodeHandle node_param("~");
 
   // check if we have to check the secret file
   string file;
-  if (!node.getParam(SECRET_FILE_PARAM, file))
+  if (!node_param.getParam(SECRET_FILE_PARAM, file))
   {
     ROS_ERROR("Parameter '%s' not found.", SECRET_FILE_PARAM);
     return MISSING_PARAMETER;
